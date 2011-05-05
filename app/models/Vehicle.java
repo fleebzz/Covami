ef90f7ea0aa00f648ededf38aabbedee848feb01
FG Ribreau <github@fgribreau.com>
2011-05-05 19:00:58 +0200

    Précision du type de Date dans Vehicle

--- app/models/Vehicle.java
+++ app/models/Vehicle.java
@@ -1,23 +1,28 @@
 package models;
 
-import java.util.*;
-import javax.persistence.*;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.Type;
 
 import play.data.validation.Required;
-import play.db.jpa.*;
+import play.db.jpa.Model;
 import tags.form.HiddenField;
 
 @Entity
 public class Vehicle extends Model {
 	@Required
 	public String registration;
-	
-//	@Required
+
+	// @Required
+	@Type(type = "date")
 	public Date techControlDate;
-	
+
 	@Required
 	@OneToOne
 	@HiddenField
 	public VehicleModel model;
-	
+
 }
