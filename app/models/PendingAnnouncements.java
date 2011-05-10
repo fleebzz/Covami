8490c6d289704b87a906bcc80a04c8230df032be
Florian Bezagu <florian.bezagu@gmail.com>
2011-05-10 14:14:28 +0200

    Mise en place des modeles pour les pending des invitations et des announcements. Impossible de gérer correctement l'héritage des modèles JPA avec Play donc 2 tables différentes.

--- app/models/PendingAnnouncements.java
+++ app/models/PendingAnnouncements.java
@@ -7,16 +7,14 @@ import play.db.jpa.Model;
 
 @Entity
 public class PendingAnnouncements extends Model {
+	@Required
+	public Long Announcement_id;
 
-//	@Required
-//	public Long Announcement_id;
-//
-//	@Required
-//	public Long applicants_id;
+	@Required
+	public Long applicants_id;
 
 	public PendingAnnouncements(Long announcementId, Long applicantId) {
-		//super(announcementId, applicantId);
-//		this.Announcement_id = announcementId;
-//		this.applicants_id = applicantId;
+		this.Announcement_id = announcementId;
+		this.applicants_id = applicantId;
 	}
 }
